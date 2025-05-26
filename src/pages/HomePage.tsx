6a2e7b1eeed2928f2f4c0990e558cc24c273d046
patriciabernardos <ptrbernardos@gmail.com>
2025-05-26 23:24:47 +0200

    fix(routes): update base paths for GitHub Pages deploy

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,8 +5,8 @@ export default function HomePage() {
 return (
     <div className="flex flex-col items-center justify-center h-screen gap-4">
      <Logo size="large" className="h-80 w-auto" />
-     <Button to={'/Login'} className="primary-button">Login</Button>
-     <Button to={'/Board'} className="primary-button">Invitado</Button>
+     <Button to={'/griffgraffgroe/login'} className="primary-button">Login</Button>
+     <Button to={'/griffgraffgroe/board'} className="primary-button">Invitado</Button>
     </div>
 );
 }
